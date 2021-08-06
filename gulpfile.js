615c75510ceee8c3893ff8759eacec9d194d7015
Darren Embry <dse@webonastick.com>
2021-08-06 12:12:23 -0400

    generate minified and unminified sass.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var autoprefixer = require('autoprefixer');
 var gulpSass = require('gulp-sass')(require('node-sass'));
 var gulpPostcss = require('gulp-postcss');
+var gulpRename = require('gulp-rename');
 
 var sassPaths = [
     'node_modules/foundation-sites/scss',
     'node_modules/motion-ui/src'
 ];
 
-function sass() {
+function sassMinified() {
     return gulp.src('scss/main.scss')
         .pipe(gulpSass({
             includePaths: sassPaths,
@@ -17,8 +18,26 @@ function sass() {
         .pipe(gulpPostcss([
             autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
         ]))
+        .pipe(gulpRename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('docs/css'));
 }
+
+function sassUnminified() {
+    return gulp.src('scss/main.scss')
+        .pipe(gulpSass({
+            includePaths: sassPaths,
+            outputStyle: 'compact' // if css compressed **file size**
+        }).on('error', gulpSass.logError))
+        .pipe(gulpPostcss([
+            autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
+        ]))
+        .pipe(gulp.dest('docs/css'));
+}
+
+var sass = gulp.parallel(sassUnminified, sassMinified);
+// var sass = sassMinified;
 
 gulp.task('sass', sass);
 gulp.task('default', sass);
