e13342083db1db3ccae04e61d70100fc3225998d
Darren Embry <dse@webonastick.com>
2024-02-25 17:44:47 -0500

    revised manga from https://texteditor.com/font-converter/
    changes G, I, Q

--- dist/js/main.js
+++ dist/js/main.js
@@ -266,9 +266,9 @@ function comic(text) {
     });
 }
 
-const MANGA_UC = Array.from('\u5342\u4e43\u531a\u15ea\u4e47\u5343\u161c\u5344' +
-                            '|\uff8c\u049c\u3125\u722a\u51e0\u3116\u5369' +
-                            '\u04a8\u5c3a\u4e02\u3112\u3129\u142f\u5c71\u4e42' +
+const MANGA_UC = Array.from('\u5342\u4e43\u531a\u15ea\u4e47\u5343\u13b6\u5344' +
+                            '\u4e28\uff8c\u049c\u3125\u722a\u51e0\u3116\u5369' +
+                            '\u024a\u5c3a\u4e02\u3112\u3129\u142f\u5c71\u4e42' +
                             '\u311a\u4e59');
 
 function manga(text) {
